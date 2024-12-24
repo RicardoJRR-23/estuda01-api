e3036ad9c2e3294dd21c96a4715de89c35c92939
Josimar <basiliojosimar@gmail.com>
2024-12-24 15:47:10 -0100

    2.1.Create Table Cronogramas

--- src/routes/private/index.js
+++ src/routes/private/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const chronogram_routes = require('./chronogram');
 const users = require('./users');
 
+//Roteadores utilizados
 router.use('/users', users);
+router.use('/chronogram', chronogram_routes);
 
 module.exports = router;
